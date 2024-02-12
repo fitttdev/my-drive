ebc8392c28ce4979ae004bc31730d85c211cf1dc
fitttdev <nima.yonten1729@gmail.com>
2024-02-12 20:24:06 +0600

    refactor router

--- app.js
+++ app.js
@@ -9,37 +9,10 @@ app.get("/", (_req, res) => {
     path.join(__dirname, "public", "index.html")
   )
 });
-// Folder CRUD
 
-// POST /folders
-app.post("/folders", (req, res) => {
-  // res.cookie("_suzal_sesiion", "ksdnvlsdnvldsvnlsjdnvsdnvjldsnvjsdlnvsdjlvjs")
-  res.status(201).json({ "msg": "Folder creation successful" });
-});
-
-// PUT /folders/:id
-app.put("/folders/:id", (req, res) => {
-  res.status(200).json({ "msg": "Folder updation successful" });
-})
-
-// GET /folders
-app.get("/folders", (req, res) => {
-  res.status(200).json(
-    { "myFolders": ["Folder 1", "Folder 2"] }
-  );
-})
-
-// GET /folder/:id
-app.get("/folders/:id", (req, res) => {
-  res.status(200).json(
-    { "folder": "Folder 1" }
-  );
-})
-
-// DELETE /folders/:id
-app.delete("/folders/:id", (req, res) => {
-  res.status(200).json({ "msg": "Folder deletion successful" });
-})
+// Routers
+const folderRouter = require('./routers/folder.router');
+app.use('', folderRouter);
 
 app.listen(port, () => {
   console.log(`App is listening on: http://localhost:${port}`);
---
+++ routers/folder.router.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+// Folder CRUD
+// POST /folders
+router.post("/folders", (req, res) => {
+  // res.cookie("_suzal_sesiion", "ksdnvlsdnvldsvnlsjdnvsdnvjldsnvjsdlnvsdjlvjs")
+  res.status(201).json({ "msg": "Folder creation successful" });
+});
+
+// PUT /folders/:id
+router.put("/folders/:id", (req, res) => {
+  res.status(200).json({ "msg": "Folder updation successful" });
+})
+
+// GET /folders
+router.get("/folders", (req, res) => {
+  res.status(200).json(
+    { "myFolders": ["Folder 1", "Folder 2"] }
+  );
+})
+
+// GET /folder/:id
+router.get("/folders/:id", (req, res) => {
+  res.status(200).json(
+    { "folder": "Folder 1" }
+  );
+})
+
+// DELETE /folders/:id
+router.delete("/folders/:id", (req, res) => {
+  res.status(200).json({ "msg": "Folder deletion successful" });
+})
+
+module.exports = router;  
