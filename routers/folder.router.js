fbe0012edd984427fdf72f8d1a5a34579703d6ce
suzWaks <suzalwakhley@gmail.com>
2024-03-09 10:34:54 +0600

    Get children folders inside parent

--- routers/folder.router.js
+++ routers/folder.router.js
@@ -47,6 +47,41 @@ router.get("/folders", async (req, res) => {
   }
 });
 
+// Get children folder by ID
+router.get("/folders/:id/children", async (req, res) => {
+  const folderId = parseInt(req.params.id);
+  try {
+    const folder = await prisma.folder.findUnique({
+      where: {
+        id: folderId
+      }
+    });
+
+    if (folder) {
+      try {
+        const childrenFolders = await prisma.folder.findMany({
+          where: { parentId: folderId }
+        });
+
+        if (childrenFolders.length > 0) { //As findMany will always return an array
+          res.status(200).json(childrenFolders);
+        } else {
+          res.status(404).json({ message: `No Folders inside parent folder with ID ${folderId}` });
+        }
+      }
+      catch (error) {
+        console.error('Error retrieving children folders:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    } else {
+      res.status(404).json({ message: `Folder with ID ${folderId} does not exist.` })
+    }
+  } catch (error) {
+    console.error('Error retrieving specific folder:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 // Get specific folder by ID
 router.get("/folders/:id", async (req, res) => {
   const folderId = parseInt(req.params.id);
@@ -60,7 +95,7 @@ router.get("/folders/:id", async (req, res) => {
     if (folder) {
       res.status(200).json(folder);
     } else {
-      res.status(404).json({message: `Folder with ID ${folderId} does not exist.`})
+      res.status(404).json({ message: `Folder with ID ${folderId} does not exist.` })
     }
   } catch (error) {
     console.error('Error retrieving specific folder:', error);
